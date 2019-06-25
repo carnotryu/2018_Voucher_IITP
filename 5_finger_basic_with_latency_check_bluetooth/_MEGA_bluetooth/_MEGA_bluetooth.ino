fa6e151c15f70ea49a0c5c512407151068dfc43c
carnotryu <carnotryu@gmail.com>
2019-06-25 17:22:08 +0900

    baud rate changed

--- 5_finger_basic_with_latency_check_bluetooth/_MEGA_bluetooth/_MEGA_bluetooth.ino
+++ 5_finger_basic_with_latency_check_bluetooth/_MEGA_bluetooth/_MEGA_bluetooth.ino
@@ -1,7 +1,7 @@
 void setup()
 {
   Serial.begin(9600);
-  Serial1.begin(38400);    // 19: MEGA_RX1 <-> BT TX, 18: MEGA_TX1 <-> BT RX
+  Serial1.begin(115200);    // 19: MEGA_RX1 <-> BT TX, 18: MEGA_TX1 <-> BT RX
 }
 
 void loop()
